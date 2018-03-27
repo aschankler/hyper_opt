8fb4410a76ab7ea80155e78de1b3b23dc65df2ba
aschankler <aschankler@haverford.edu>
2018-03-27 00:47:45 -0400

    Make launcher construction explicit.

--- run_job.py
+++ run_job.py
@@ -1,6 +1,36 @@
 
 import os
-import pylauncher
+from pylauncher import JobId, FileCommandlineGenerator, TaskGenerator, FileCompletion
+from pylauncher import HostPool, HostListByName, SSHExecutor
+from pylauncher import LauncherJob
 
-test_path = os.path.join(os.getcwd(), 'pylauncher/examples/commandlines')
-pylauncher.ClassicLauncher(test_path, debug="job")
+job_file = os.path.join(os.getcwd(), 'pylauncher/examples/commandlines')
+
+jobid = JobId()
+debug = "job+host"
+workdir = "pylauncher_tmp" + str(jobid)
+cores = 1
+
+# This generates a list of command lines to run
+cmd_gen = FileCommandlineGenerator(job_file, cores=cores, debug=debug)
+
+# Wraps command line generators with info about whether tasks have completed/how to run the task
+task_gen = TaskGenerator(cmd_gen,
+                         completion=lambda x: FileCompletion(taskid=x, stamproot="expire", stampdir=workdir),
+                         debug=debug)
+
+# Wraps a commandline, writes to file, creates new commandline to exec the file.
+# This is where envt is exported, may need to modify
+executor = SSHExecutor(workdir=workdir, debug=debug)
+
+host_pool = HostPool(hostlist=HostListByName(), commandexecutor=executor, debug=debug)
+
+
+# Also has a "maxruntime" param, a "gather_output" param
+job = LauncherJob(
+    hostpool=host_pool,
+    taskgenerator=task_gen,
+    debug=debug)
+
+job.run()
+print job.final_report()
